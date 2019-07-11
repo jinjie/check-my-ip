ef6eb6fc0ae8b67363e3a29826ca5ceb445eea4e
Kong Jin Jie <jinjie@swiftdev.sg>
2019-07-11 11:24:58 +0800

    First import

---
+++ README.md
@@ -0,0 +1,14 @@
+## Checks WAN IP Change
+
+Small script to check my WAN IP change. If a change was detected, email me.
+
+### Installation
+
+```bash
+curl https://raw.githubusercontent.com/jinjie/check-my-ip/master/check-my-ip.sh -o check-my-ip.sh
+chmod +x check-my-ip.sh
+```
+
+### Usage
+
+`./check-my-ip.sh ns1.google.com myemail@example.com`
---
+++ check-my-ip.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ "$1" = "" ] || [ "$2" = "" ]; then
+  echo "Usage: ./check-my-ip <DYN_HOSTNAME> <MY_EMAIL>" >&2
+  exit 1;
+fi
+
+DYN_HOSTNAME="$1"
+MY_EMAIL="$2"
+
+LAST_WAN_IP_FILE="$HOME/.last-known-wanip-$DYN_HOSTNAME"
+
+if ! [ -x "$(command -v dig)" ]; then
+  echo "DiG not found. Aborting." >&2
+  exit 1;
+fi
+
+if ! [ -x "$(command -v mail)" ]; then
+  echo "Mailx not found. Aborting." >&2
+  exit 1;
+fi
+
+if [ -f "$LAST_WAN_IP_FILE" ]; then
+  LAST_WAN_IP=`cat $LAST_WAN_IP_FILE`
+  echo Last WAN IP: $LAST_WAN_IP
+else
+  echo Last WAN IP: Unknown
+  touch "$LAST_WAN_IP_FILE"
+fi
+
+WAN_IP=`dig @8.8.8.8 $DYN_HOSTNAME +short`
+
+echo Current WAN IP: $WAN_IP
+
+if [ "$LAST_WAN_IP" != "$WAN_IP" ]; then
+  echo IP Change Detected
+  echo -n $WAN_IP > $LAST_WAN_IP_FILE
+
+  mail -s "$DYN_HOSTNAME IP Address Changed To $WAN_IP" $MY_EMAIL <<EOF
+    Hello.
+
+    This is to inform you that your IP address was changed to $WAN_IP from $LAST_WAN_IP
+EOF
+fi
